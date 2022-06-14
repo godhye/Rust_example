048436304fbef80c3010a055cd3843c79587ede9
godhye <exmr12@gmail.com>
2022-06-15 01:30:04 +0900

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,60 @@
-extern crate rand;
 
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+#[derive(Debug)]
+enum UsState{   
+    Alabama,
+    Alaska,
+    Ny,
 
-fn main() {
+}
+enum Coin{
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: &Coin)-> u32{
+    
+    match coin{
+        Coin:: Penny => 1,
+        Coin:: Nickel => 5,
+        Coin:: Dime => 10,
+        Coin:: Quarter(state) => {
+            25   
+        },
+    } 
+}
+
+fn plus_1(x: Option<i32>) -> Option<i32>{
+
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+
+    }
+}
+
+fn main(){
+
+    let coin = Coin::Quarter(UsState::Ny);
+
+    
+    
+    if let Coin::Quarter(state) = coin {
+        println!("State Quater from {:?}!", state);
+    } 
+
+    let total = value_in_cents(&coin);
 
 
-    let mut fah = String::new();
+    let some_num = Some(5);
+    let plusone = plus_1(some_num);
 
-    io::stdin().read_line(&mut fah).expect("error to std in");
+    if let Some(6) = plusone{
+        println!("6");
+    }
+    let absent_num:Option<i32> = None;
 
-   let fah :f32 = fah.trim().parse().expect("Invalid Input");
 
-    let Cel = (fah - 32.0 )/ 1.8000;
-
-    println!("화씨 {} -> 섭씨{} ",fah , Cel);
-
-    let Fah = Cel * 1.8000 + 32.0;
-
-    println!("섭씨 {} -> 화씨{} ",Cel , Fah);
 
 }
